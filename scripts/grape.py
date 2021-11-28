d49360538521d03893219e27324bc33146d43b07
nikostsagk <n.tsagko@gmail.com>
2021-11-28 19:54:27 +0000

    working for now...

--- scripts/grape.py
+++ scripts/grape.py
@@ -39,52 +39,62 @@ class image_converter:
         # imsgmsg to cv2
         cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
 
-        # Covnert to HSV
-
         # Filter out anything but purple
-        purple_mask = self.colour_filter(cv_image)#cv2.inRange(hsv_image, (100, 20, 20), (165, 255, 255))
+        purple_mask = self.colour_filter(cv_image)
 
         # Debug
-        kernel = np.ones((3,3), dtype=np.uint8)
-        debug = cv2.morphologyEx(purple_mask, cv2.MORPH_CLOSE, kernel)
-        #debug = cv2.morphologyEx(debug, cv2.MORPH_CLOSE, kernel)
+        kernel = np.ones((11,11), dtype=np.uint8)
+        kernel2 = np.ones((3,3), dtype=np.uint8)
+        debug = purple_mask
+        debug = cv2.morphologyEx(debug, cv2.MORPH_CLOSE, None)
+        debug = cv2.morphologyEx(debug, cv2.MORPH_DILATE, kernel)
+        #debug = cv2.morphologyEx(debug, cv2.MORPH_OPEN, None)
+        purple_mask = cv2.morphologyEx(purple_mask, cv2.MORPH_OPEN, kernel2)
 
-        #dist = cv2.distanceTransform(debug, cv2.DIST_L2, 3)
-        #cv2.normalize(dist, dist, 0, 1.0, cv2.NORM_MINMAX)
-        #_, dist = cv2.threshold(dist, 0.4, 1.0, cv2.THRESH_BINARY)
-        #kernel = np.ones((3,3), dtype=np.uint8)
-        #dist = cv2.dilate(dist, kernel)
-        #dist_8u = dist.astype("uint8")
+        _, contours, _ = cv2.findContours(debug, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        markers = np.zeros(debug.shape, dtype=np.int32)
 
+        centers = []
+        colors = []
+        for i, c in enumerate(contours):
+            area = cv2.contourArea(c)
+            if area > 500:
+                M = cv2.moments(c)
+                cX = int(M["m10"]/M["m00"])
+                cY = int(M["m01"]/M["m00"])
+                centers.append((cX, cY))
+                #cv2.drawContours(markers, [c], -1, (i+1), -1)
+                cv2.circle(markers, (cX,cY), 10, (i+1), -1)
+                colors.append((rng.randint(0, 256), rng.randint(0,256), rng.randint(0,256)))
+        markers_8u = (markers*10).astype("uint8")
 
-        #_, contours, _ = cv2.findContours(dist_8u, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        #markers = np.zeros(dist.shape, dtype=np.int32)
-        #for i in range(len(contours)):
-        #    cv2.drawContours(markers, contours, i, (i+1), -1)
-        #cv2.circle(markers, (5,5), 1, (255,255,255), -1)
-        #markers_8u = (markers*10).astype("uint8")
+        purple_mask_rgb = np.dstack([purple_mask, purple_mask, purple_mask])
+        purple_mask_3d = np.dstack([purple_mask, purple_mask, purple_mask])
+        
+        cv2.watershed(purple_mask_rgb, markers)
 
-        #imgResult = np.dstack([purple_mask, purple_mask, purple_mask])
-        #cv2.watershed(imgResult, markers)
+        labels = np.unique(markers)
+        for n, label in enumerate(labels[1::]):
+            ix, iy = np.where(markers == label)
+            purple_mask_rgb[ix,iy,:] = colors[n]
 
-        #colors = []
-        #for contour in contours:
-        #    colors.append((rng.randint(0, 256), rng.randint(0,256), rng.randint(0,256)))
-
-        #dst = np.zeros((markers.shape[0], markers.shape[1], 3), dtype=np.uint8)
-        #for i in range(markers.shape[0]):
-        #    for j in range(markers.shape[1]):
-        #        index = markers[i,j]
-        #        if index > 0 and index <= len(contours):
-        #            dst[i,j,:] = colors[index-1]
-
-        #dst = cv2.bitwise_and(dst, np.dstack([purple_mask, purple_mask, purple_mask]))
+        debug = cv2.bitwise_and(purple_mask_rgb, purple_mask_3d)
+         
+        bboxes = []
+        for n, color in enumerate(colors):
+            ix, iy = np.where(np.all(debug == color, axis=-1))
+            try:
+                x0, x1 = min(ix), max(ix)
+                y0, y1 = min(iy), max(iy)
+                cv2.rectangle(cv_image, (y0, x0), (y1, x1), color, thickness=3)
+            except:
+                continue
 
         # cv2 to imgmsg
         purple_mask_msg = self.bridge.cv2_to_imgmsg(purple_mask, "mono8")
         purple_mask_msg.header = data.header
 
-        debug_msg = self.bridge.cv2_to_imgmsg(debug, "mono8")
+        debug_msg = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
         debug_msg.header = data.header
 
         # publish masks
