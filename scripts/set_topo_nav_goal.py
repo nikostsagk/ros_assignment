234e29c89427c9e66af4c55f3c8d200b57aa2d6e
nikostsagk <n.tsagko@gmail.com>
2022-01-24 21:38:24 +0000

    Add ros_numpy in deps, new nav_client_config, and minor cleanup

--- scripts/set_topo_nav_goal.py
+++ scripts/set_topo_nav_goal.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import yaml
 import rospy
 import actionlib
 import message_filters
@@ -12,52 +13,44 @@ from topological_navigation.msg import GotoNodeAction, GotoNodeGoal
 class topoNavClient:
 
     def __init__(self):
-        self.client = actionlib.SimpleActionClient('/thorvald_001/topological_navigation', GotoNodeAction)
-        self.client.wait_for_server()
 
         # Init
         self.rgb_image = None
         self.depth_image = None
-        self.image_topic = rospy.get_param("~image_topic", "/thorvald_001/kinect2_front_camera/hd/image_color_rect")
-        self.depth_topic = rospy.get_param("~depth_topic", "/ros_assignment/depth_registered/image_rect")
+        self.image_topic = rospy.get_param("~image_topic", "/camera/rgb/image_color_rect")
+        self.depth_topic = rospy.get_param("~depth_topic", "/camera/depth/image_rect")
+
+        print(self.image_topic, self.depth_topic)
         subscribers = [
                 message_filters.Subscriber(self.image_topic, Image),
                 message_filters.Subscriber(self.depth_topic, Image),
                 ]
         self.ts = message_filters.ApproximateTimeSynchronizer(subscribers, 1, 0.1, allow_headerless=True)
         self.ts.registerCallback(self.image_callback)
-    
-        goals = [
-                "r3-c0", 
-                "r3-c1",
-                "r3-c2", 
-                "r3-c3", 
-                "r3-c4", 
-                "r3-c5", 
-                "r3-c6", 
-                "r3-c7", 
-                "r3-c8", 
-                "r3-c9", 
-                "r3-c10", 
-                "r3-c11", 
-                "r3-c12",
-                "r3-c13",
-                "r3-c14",
-                "r3-c15",
-                "r3-c16",
-                "r3-c17",
-                "r3-c18",
-                "r3-c19",
-                "r3-c20"]
 
-        self.main(goals)
-    
+        self.yaml_file = rospy.get_param("~nav_client_file", None)
+        self.config = self.load_nav_file(self.yaml_file)
+
+        # Start topo_nav client
+        self.client = actionlib.SimpleActionClient('/{:s}/topological_navigation'.format(self.config["robot"]), GotoNodeAction)
+        self.client.wait_for_server()
+
+        self.main()
+
     def image_callback(self, rgb_data, depth_data):
         self.rgb_image = rgb_data
         self.depth_image = depth_data
 
-    def main(self,goals):
-        for g in goals:
+    def load_nav_file(self, file):
+        with open(file, 'r') as f:
+            try:
+                yaml_file = yaml.safe_load(f)
+                return yaml_file
+            except yaml.YAMLError as e:
+                print(e)
+
+    def main(self):
+        for g in self.config["rows"]:
             goal = GotoNodeGoal()
             goal.target = g
             self.client.send_goal(goal)
